9718d23fb03191d42d53e6f125bd933b1aeb51d0
leandro.souza-at-766073738479 <lramosouza@outlook.com>
2018-04-09 10:12:14 -0300

    Innitial Commit

---
+++ src/test/java/br/com/tasteplace/TastePlaceConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.tasteplace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TastePlaceConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
